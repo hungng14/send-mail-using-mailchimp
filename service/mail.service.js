644b435dce0698dc6aed80f2d4abea3f60cedc12
hungng14 <hungnguyendnipr101196@gmail.com>
2023-12-08 17:34:48 +0700

    up

--- service/mail.service.js
+++ service/mail.service.js
@@ -2,12 +2,12 @@ import { mailchimp } from "../mail/provider.js";
 
 export const sendMessage = async () => {
   const message = {
-    from_email: 'hung.nuro0@gmail.com',
+    from_email: 'from0@gmail.com',
     subject: 'Hello world',
     text: 'Welcome to Mailchimp Transactional!',
     to: [
       {
-        email: 'to@coderpush.com',
+        email: 'to@example.com',
         type: 'to',
       },
     ],
